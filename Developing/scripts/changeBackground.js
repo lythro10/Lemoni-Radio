33475b00f7bfb29207b8a661aaad34f9f1adb7cb
George <george.michael45@gmail.com>
2020-03-27 20:20:56 +0200

    footer fix

--- Developing/scripts/changeBackground.js
+++ Developing/scripts/changeBackground.js
@@ -117,6 +117,7 @@ if (6 <= hour && hour < 12) {
     var chatTitleColor = "#fbe246";
     var zoneH1text = "Energetic Zone";
     var zonePtext = "Η energetic zone ανοίγει στις 6 τα ξημερώματα και κλείνει στις 12 το μεσημέρι προσφέροντας μια ενεργητική ατμόσφαιρα και μια δυναμική ροή, ικανή να σας κρατάει σε μια σχετική εγρήγορση. Ξεκινήστε τη μέρα σας με νερό και λίγες σταγόνες λεμόνι, φτιάξτε καφέ, και συντονιστείτε με το λεμόνι!.. θα σας κρατήσει συντροφιά με εξαίσιες μουσικές και λόγια, συντονισμένα στους πρωινούς βιορυθμούς.";
+    var zoneFooterText = "#5bb646";
 }
 // 12pm - 6pm Mellow Zone  
 if (12 <= hour && hour < 18) {
@@ -134,7 +135,7 @@ if (12 <= hour && hour < 18) {
     var chatTitleColor = "#d46f97";
     var zoneH1text = "Mellow Zone";
     var zonePtext = "Η mellow zone ανοίγει τις πόρτες της στις 12 το μεσημέρι και κλείνει στις 6 το απόγευμα. Τρέξαμε αρκετά το πρωί και έχουμε ανάγκη να ρίξουμε λίγο τους ρυθμούς μας, να κατευνάσουμε τα πνεύματα, να πάρουμε ανάσες και δυνάμεις για τη συνέχεια! Συντονιστείτε με το λεμόνι!.. θα σας κρατήσει συντροφιά με εξαίσιες μουσικές και λόγια, συντονισμένα στους μεσημεριανούς και απογευματινούς βιορυθμούς.";
-    
+    var zoneFooterText = "#d46f97";
    
 }
 // 6pm - 12am Twilight Zone 
@@ -153,7 +154,7 @@ if (18 <= hour && hour <= 23) {
     var chatTitleColor = "#bf1e2e";
     var zoneH1text = "Twilight Zone";
     var zonePtext = "Η twilight zone υποδέχεται το κοινό της στις 6 το απόγευμα και αποχαιρετά τους τελευταίους πελάτες της, στις 12 τα μεσάνυχτα. Η ενεργητικότητα επιστρέφει, το κύμα ανεβαίνει, αλλά αυτή τη φορά το σκηνικό είναι πιο σκοτεινό. Όσο προχωρεί η ώρα προς τα μεσάνυχτα, τα αίματα ανάβουν λίγο παραπάνω, μια μαργαρίτα και λεμόνι για παρέα, με εξαίσιες μουσικές και λόγια, συντονισμένα στους απογευματινούς και νυχτερινούς βιορυθμούς";
-
+    var zoneFooterText = "#ee7423";
 
 }
 // 12am - 6am Deep Zone
@@ -171,6 +172,7 @@ if (0 <= hour && hour < 6) {
     var chatTitleColor = "#7f3f98";
     var zoneH1text = "Deep Zone";
     var zonePtext = "Η deep zone είναι το dance club του πολυχώρου και ο χορός εδώ, πρωταγωνιστεί, είναι το βασικό ζητούμενο τουλάχιστον τις πρώτες τρεις ώρες που οι ηλεκτρονικές μουσικές του λεμονιού, διεγείρουν τα σώματα να κινηθούν και να κουνηθούν. Όπως όλα στο λεμόνι, σταδιακά μεταμορφώνεται  και η deep zone, και κάπου κοντά στα ξημερώματα γίνεται  ένα τύπου relaxing club! Όσοι/ες μένετε ξύπνιοι στη γοητεία της νύχτας, το λεμόνι προσφέρει μια σαγηνευτική ροή με εξαίσιες μουσικές και λόγια συντονισμένα στους βιορυθμούς της βαθιάς νύχτας!";
+    var zoneFooterText = "#2c2869";
 }
 
 
@@ -198,6 +200,7 @@ document.getElementsByClassName("scriptColor")[7].style.color = textColor1;
 document.getElementById("chatText").style.color = chatTitleColor;
 document.getElementById("zoneH1").innerHTML = zoneH1text;
 document.getElementById("zoneP").innerHTML = zonePtext;
+document.getElementById("footerText").style.color = zoneFooterText;
 var bgColor = "repeating-linear-gradient(" + "90deg" + "," + x + "," + y + ")";
 document.body.style.background = bgColor;
 
